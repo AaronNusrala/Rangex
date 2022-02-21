260b3157901053fb60c1b2a1abe143dc994a5b71
AaronNusrala <aaronnusrala@gmail.com>
2022-02-20 21:13:30 -0500

    Adding test project and RageCalculator tests.

---
+++ RegexGeneratorTests/RangeCalculatorTests.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RegexGenerator;
+using RegexGenerator.Models;
+
+namespace RegexGeneratorTests
+{
+    public class Tests
+    {
+        private RangeCalculator _rangeCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rangeCalculator = new RangeCalculator();
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Generates_Ranges(int min, int max, (int Min, int Max)[] expectedRanges)
+        {
+            var actualRanges = _rangeCalculator.CalculateRanges(min, max).ToList();
+            var actualRangeList = actualRanges.ToList();
+
+            if (expectedRanges.Length != actualRangeList.Count)
+            {
+                var message = CreateMessage(expectedRanges, actualRangeList);
+                throw new AssertionException(message);
+            }
+
+            foreach (var (expectedMin, expectedMax) in expectedRanges)
+            {
+                var actualRange = actualRangeList.FirstOrDefault(r => r.Min == expectedMin && r.Max == expectedMax);
+
+                if (actualRange == null)
+                {
+                    var message = CreateMessage(expectedRanges, actualRanges);
+                    throw new AssertionException(message);
+                }
+
+                actualRangeList.Remove(actualRange);
+            }
+        }
+
+        private static string CreateMessage(IEnumerable<(int Min, int Max)> expectedRanges, IEnumerable<RegexRange> actualRanges)
+        {
+            var sb = new StringBuilder();
+            sb.Append("Expected: ");
+
+            foreach (var (min, max) in expectedRanges.OrderBy(r => r.Min))
+            {
+                sb.Append("(").Append(min).Append(", ").Append(max).Append(") ");
+            }
+
+            sb.AppendLine();
+            sb.Append("Got:      ");
+
+            foreach (var actualRange in actualRanges.OrderBy(r => r.Min))
+            {
+                sb.Append("(").Append(actualRange.Min).Append(", ").Append(actualRange.Max).Append(") ");
+
+            }
+
+            return sb.ToString();
+        }
+
+        private static object[] TestCases =
+        {
+            new object[] { 0, 0, new []{ (0, 0) } },
+            new object[] { 0, 1, new []{ (0, 1) } },
+            new object[] { 1, 1, new []{ (1, 1) } },
+            new object[] { 1, 2, new []{ (1, 2) } },
+            new object[] { 1, 10, new []{ (1, 9), (10, 10) } },
+            new object[] { 1, 100, new []{ (1, 9), (10, 99), (100, 100) } },
+            new object[] { 1, 101, new []{ (1, 9), (10, 99), (100, 101) } }
+        };
+    }
+}
